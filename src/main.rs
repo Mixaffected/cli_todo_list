d9c4847d7b30c435c40c2bad795100b0891c938b
Mixaffected <marvin.dziedzina@gmail.com>
2023-10-28 23:26:42 +0200

    Added base code

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ fn main() {
 
         let args = get_args(args);
 
+        for arg in &args {
+            println!("{}", arg);
+        }
+
         match args[0].as_str() {
             "exit" => break,
             "help" => print_help(),
@@ -19,16 +23,41 @@ fn main() {
     }
 }
 
+fn show_todo_entries(args: Vec<String>) {}
+
+fn add_todo_entry(args: Vec<String>) {}
+
+fn remove_todo_entry(args: Vec<String>) {}
+
 fn print_help() {
     println!("No help here!");
 }
 
-fn get_args(args: String) -> Vec<String> {
+// convert string to args
+fn get_args(string_args: String) -> Vec<String> {
     let mut split_args: Vec<String> = Vec::new();
-    let split_string = args.split_whitespace();
-    for arg in split_string {
-        split_args.push(String::from(arg));
+
+    let mut current_part: String = String::new();
+    let mut is_string: bool = false;
+    for arg_char in string_args.trim().chars() {
+        if arg_char == '"' {
+            is_string = !is_string;
+            continue;
+        }
+
+        if is_string {
+            current_part.push(arg_char);
+            continue;
+        } else if arg_char == ' ' {
+            split_args.push(current_part.clone());
+            current_part = String::new();
+            continue;
+        }
+
+        current_part.push(arg_char);
     }
+
+    split_args.push(current_part);
 
     return split_args;
 }
